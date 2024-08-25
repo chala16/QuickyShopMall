720ca5bc7553c66aad9d8f35823d661cf9b13a77
chala16 <chalanakaveeshami@gmail.com>
2024-08-25 18:19:54 +0530

    add footer and slider

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import NavBar from "./Navbar";
 import "react-toastify/dist/ReactToastify.css";
+import ImageMover from "../mover/ImageMover";
+import Footer from "../footer/Footer";
 
 const Home = () => {
   return (
     <div>
       <NavBar />
+      <ImageMover />
       Home
+      <Footer />
     </div>
   );
 };
