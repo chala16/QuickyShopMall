643040403f6860941ad6d72fbd5892d172194531
Savishka Dilshan <savishkadilshanofficial@gmail.com>
2024-08-27 07:14:33 +0530

    Sorting wishlist items according to the shopFloorNo
    Add sort() for getItemsFromWishlist method to sort wishlist items.

--- backend/src/controller/wishlistController.js
+++ backend/src/controller/wishlistController.js
@@ -1,14 +1,14 @@
-const Wishlist = require("../models/Wishlist")
-const Item = require("../models/inventory")
+const Wishlist = require("../models/Wishlist");
+const Item = require("../models/inventory");
 
 const addItemsToWishlist = async (req, res) => {
-  const userId = req.user._id
-  const { itemId } = req.body
+  const userId = req.user._id;
+  const { itemId } = req.body;
 
   try {
-    const item = await Item.findById(itemId)
+    const item = await Item.findById(itemId);
     if (!item) {
-      return res.status(404).json({ error: "Item not found" })
+      return res.status(404).json({ error: "Item not found" });
     }
 
     const updatedWishlist = await Wishlist.findOneAndUpdate(
@@ -26,48 +26,50 @@ const addItemsToWishlist = async (req, res) => {
 
     res
       .status(200)
-      .json({ message: "Item added to wishlist", updatedWishlist })
+      .json({ message: "Item added to wishlist", updatedWishlist });
   } catch (error) {
-    res.status(500).json({ error: error.message })
+    res.status(500).json({ error: error.message });
   }
-}
+};
 
 const getItemsFromWishlist = async (req, res) => {
-  const userId = req.user._id
+  const userId = req.user._id;
 
   try {
     const wishlist = await Wishlist.findOne({ userId })
       .populate({
-        path: 'items.itemId',
-        select: 'name price inStock image',
+        path: "items.itemId",
+        select: "name price inStock image",
       })
       .populate({
-        path: 'items.shopId',
-        select: 'shopName shopFloorNo',
+        path: "items.shopId",
+        select: "shopName shopFloorNo",
       });
 
     if (!wishlist || wishlist.items.length === 0) {
       return res
         .status(200)
-        .json({ message: "There are no items in your wishlist." })
+        .json({ message: "There are no items in your wishlist." });
     }
 
-    const items = wishlist.items.map(item => ({
-      name: item.itemId.name,
-      price: item.itemId.price,
-      inStock: item.itemId.inStock,
-      image: item.itemId.image,
-      shopName: item.shopId.shopName,
-      shopFloorNo: item.shopId.shopFloorNo,
-    }));
+    const items = wishlist.items
+      .map((item) => ({
+        name: item.itemId.name,
+        price: item.itemId.price,
+        inStock: item.itemId.inStock,
+        image: item.itemId.image,
+        shopName: item.shopId.shopName,
+        shopFloorNo: item.shopId.shopFloorNo,
+      }))
+      .sort((a, b) => a.shopFloorNo - b.shopFloorNo);
 
-    res.status(200).json({ items })
+    res.status(200).json({ items });
   } catch (error) {
-    res.status(500).json({ error: error.message })
+    res.status(500).json({ error: error.message });
   }
-}
+};
 
 module.exports = {
   addItemsToWishlist,
-  getItemsFromWishlist
-}
+  getItemsFromWishlist,
+};
