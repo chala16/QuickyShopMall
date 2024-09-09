def0dbc90ca4b1737e6d4b51d4cbd19866040e58
chala16 <chalanakaveeshami@gmail.com>
2024-09-09 13:01:13 +0530

    use uselocation to update discount price
    when click a discount item then in the page of item details shows discount price in that item details page using useLocation

--- frontend/src/pages/client/ViewItemDetails.jsx
+++ frontend/src/pages/client/ViewItemDetails.jsx
@@ -6,15 +6,16 @@ import AddWishlistButton from "../../components/wishlist/AddWishlistButton";
 import ReviewForm from "../../components/review/ReviewForm";
 import ReviewList from "../../components/review/ReviewList";
 import { useAuthContext } from "../../hooks/useAuthContext"; // Import your auth context hook
-
+import { useLocation } from 'react-router-dom';
 
 const ViewItemDetails = () => {
   const { id } = useParams();
   const [item, setItem] = useState(null);
   const { user } = useAuthContext(); // Get user info from context
   const [reviewsUpdated, setReviewsUpdated] = useState(false); // State to trigger review list refresh
-
-
+  const location = useLocation();
+  const { discountPrice } = location.state || {};
+  
   useEffect(() => {
     if (id) {
       fetch(`http://localhost:3000/home/get-item/${id}`, {
@@ -62,7 +63,7 @@ const ViewItemDetails = () => {
             className="object-contain mb-4 h-96 w-full max-w-[200px] md:max-w-[35%]"
           />
           <div className="flex-1 md:ml-8">
-            <p className="mb-2 font-bold text-red-500">Price: Rs. {item.price}</p>
+            <p className="mb-2 font-bold text-red-500">Price: Rs. {discountPrice}</p>
             <p className="mb-2 text-sm text-gray-600">Category: {item.category}</p>
             <p className="text-sm text-gray-500">Description: {item.description}</p>
 
